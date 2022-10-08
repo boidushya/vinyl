3e1ae9c9bd976e02cb6e8237ec5859a4f35fb80e
yash-wins <yashwinsaini@gmail.com>
2022-10-08 12:28:31 +0530

    Added docstrings for `room_handler`

--- app/core/room_handler.py
+++ app/core/room_handler.py
@@ -4,10 +4,27 @@ from redis import Redis
 class Room:
     # All room related operations
     def add_member(self, room_id: str, user_id: str, redis_client: Redis):
+        """Add a new member to an existing room
+
+        Args:
+            room_id (str): room id where the new user is to be added
+            user_id (str): user id of the new user
+            redis_client (Redis): redis client to handle queries
+        """
         redis_client.lpush(f"room:{room_id}:members", user_id)
         redis_client.zadd(f"room:{room_id}:leaderboard", {user_id: 0})
 
     def update_score(self, scores: dict, room_id: str, redis_client: Redis):
+        """update score of a room after every round
+
+        Args:
+            scores (dict): score of each player to be incremented
+            room_id (str): room id where score needs to be updated
+            redis_client (Redis): redis client to handle the query
+
+        Returns:
+            dict: scoreboard of the room
+        """
         for player in scores.items():
             user_id, score_incr = player
             redis_client.zincrby(f"room:{room_id}:leaderboard", score_incr, user_id)
@@ -34,6 +51,16 @@ class Room:
         redis_client.setbit(f"room:{room_id}", 1, 0)
 
     def remove_player(self, room_id: str, user_id: str, redis_client: Redis):
+        """Remove a player from existing room
+
+        Args:
+            room_id (str): room id from player needs to be removed
+            user_id (str): user id of user to be removed
+            redis_client (Redis): redis client to handle the query
+
+        Returns:
+            dict: remaining players in the room
+        """
         redis_client.lrem(f"room:{room_id}:members", 0, user_id)
 
         return redis_client.lrange(f"room:{room_id}:members", 0, -1)
